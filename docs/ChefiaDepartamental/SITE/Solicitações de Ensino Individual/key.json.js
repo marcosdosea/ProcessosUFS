a5f68266e3ad4d071d4563da771d5204dac78dae
esodegemeos <esodegemeos@gmail.com>
2019-11-27 11:23:01 -0300

    Novos arquivos adicionados

---
+++ docs/ChefiaDepartamental/SITE/Solicitações de Ensino Individual/key.json.js
@@ -0,0 +1,16 @@
+/********************************************************
+    @Publish Web Attributes
+********************************************************/
+PublishWebAttributes = {
+	useTheme: false,			// If you want personalize your export styles use theme builder for bizagi and copy the css file in /libs/css/theme.css
+	preserveHTML: true,			// If you want preserve your properties / description and attributes html format
+	showPublishDate: false,			// If you want to show your publish date
+	subNavigation: false,			// If you want to show a subnavigation bar for properties
+	showPerformersList: false,		// If you want to show a performers link
+	maxLenghtTableViewDescription: 125,     // Number of characters in description
+	maxLenghtDescription: 1000,		// Limits of characters in description (dialog)
+	ie8:{
+			draggable: false,
+        	diagramsSupported: 38
+	}
+};
