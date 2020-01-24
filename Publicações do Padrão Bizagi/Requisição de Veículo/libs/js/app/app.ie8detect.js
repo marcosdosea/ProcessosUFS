5f26b1cd442d91fa304eace173d909eb9d1539b3
esodegemeos <esodegemeos@gmail.com>
2020-01-24 02:47:36 -0300

    Novos arquivos adicionados para publicação

---
+++ Publicações do Padrão Bizagi/Requisição de Veículo/libs/js/app/app.ie8detect.js
@@ -0,0 +1,23 @@
+
+
+function IsIE8Browser() {
+    var rv = -1;
+    var ua = navigator.userAgent;
+    var re = new RegExp("Trident\/([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null) {
+        rv = parseFloat(RegExp.$1);
+    }
+    return (rv == 4);
+}
+
+if (IsIE8Browser()) {
+    var htmlObj = document.getElementsByTagName('html')[0];
+        htmlObj.className = htmlObj.className + ' no-js lt-ie9';
+
+    var element = document.getElementById('biz-ex-support');
+    if(Bizagi.App.Model.texts){
+        element.innerHTML = Bizagi.AppModel.texts['unsupported'];
+    }else{
+        element.innerHTML = 'unsupported';
+    }
+}
